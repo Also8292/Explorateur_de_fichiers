67725106136c6dddae41092edd55af4abbeb284c
Aliou-Sow <sowalils1992@gmail.com>
2018-10-04 10:28:48 +0000

    style folder

--- post.php
+++ post.php
@@ -17,12 +17,21 @@ function show_folders() {
 
 function test($folder_path) {
     if($dossier = opendir($folder_path)) {
+        ?>
+
+        <div style="width: 100%; background-color: #c1f8f5; margin-bottom: 10px">
+            <?= $folder_path ?>
+        </div>
+
+        <?php
+        
+
         while(false !== $fichier = readdir($dossier)) {
             if($fichier != '.' && $fichier != "..") {
                 if(is_dir($folder_path . '' . $fichier)) {
                     if(isset($_GET['folder'])) {
                         ?>
-                        <div class="col-md-2">
+                        <div class="col-md-2 file_zone">
                             <a href="index.php?folder=<?= $_GET['folder']  . '/' . $fichier ?>" class="folder_style">
                                 <img src="public/images/icone_dossier.png" alt="" width="30" height="30">
                                 <p style="font-size: 14px;"><?= $fichier ?></p>
@@ -33,7 +42,7 @@ function test($folder_path) {
 
                     else {
                         ?>
-                        <div class="col-md-2">
+                        <div class="col-md-2 file_zone">
                             <a href="index.php?folder=<?= $fichier ?>">
                                 <img src="public/images/icone_dossier.png" alt="" width="30" height="30">
                                 <p style="font-size: 14px;"><?= $fichier ?></p>
@@ -44,7 +53,7 @@ function test($folder_path) {
                 }
                 else {
                     ?>
-                        <div class="col-md-2">
+                        <div class="col-md-2 file_zone">
                             <img src="public/images/file.png" alt="" width="40" height="40">
                             <p style="font-size: 14px;"><?= $fichier ?></p>
                         </div>
